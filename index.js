21ee3d96684f00b0c6c7d9d1736e9360959a2ea9
hydRAnger <armyiljfe@gmail.com>
2016-02-17 11:20:03 +0800

    D20 dices

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var args = process.argv.slice(2);
+var sideCount = parseInt(args[0]);
+var rollCount = parseInt(args[1]);
+
+if (Number.isInteger(sideCount) && Number.isInteger(rollCount)) {
+  if ([4, 6, 8, 10, 12, 20, 100].indexOf(sideCount) !== -1) {
+    console.log('roll ' + sideCount + ' side dice for ' + rollCount + ' times.');
+    for (var i = 0; i < rollCount; i++) {
+      console.log(Math.ceil(Math.random() * sideCount));
+    }
+  } else {
+    console.log('not a D20 dice (D4, D6, D8, D10, D12, D20, D100)');
+  }
+} else {
+  console.log('to use: ./dice sideCount rollCount');
+}
